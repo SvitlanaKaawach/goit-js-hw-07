74b7a08d7bcea5a0e9839ef3b2024b10248962f0
Svitlana Kaawach <svitlanakaawach@gmail.com>
2022-01-10 13:46:36 +0200

    ex- 2

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
+
+const galleryElements = galleryItems.map((item) =>
+    `<li>
+     <a class="gallery__item" href="${item.original}">
+        <img class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+        />
+     </a>
+</li>
+`
+).join("");
+
+
+gallery.insertAdjacentHTML("beforeend", galleryElements);
+
+
+let lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionPosition: "bottom", captionDelay: 250 });
+
+console.log(gallery);
 console.log(galleryItems);
