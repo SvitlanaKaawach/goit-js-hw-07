f4601230a7a8e18f3906e52bc564d6befd8f32e7
Svitlana Kaawach <svitlanakaawach@gmail.com>
2022-01-10 13:26:59 +0200

    ex-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,59 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+
+function galleryContainer(arr) {
+    const divEl = arr.map((item) =>
+        `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img loading='lazy' class="gallery__image"
+            src="${item.preview}"
+            data-src="${item.original}"
+            alt="${item.desciption}"
+            />
+        </a>
+    </div>
+    `).join("");
+    return divEl;
+};
+const imagesList = galleryContainer(galleryItems);
+
+gallery.innerHTML = imagesList;
+
+
+let instance = basicLightbox.create(
+    `<img src="" width="800" height="600">`,
+    {
+        onShow: instance => {
+            window.addEventListener('keydown', handleEscModalClose)
+            console.log(instance);
+        },
+        onClose: instance => {
+            window.removeEventListener('keydown', handleEscModalClose)
+            console.log(instance);
+        },
+
+    });
+
+
+gallery.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.className === "gallery__image") {
+        const srcLink = e.target.dataset.src;
+        instance.element().querySelector('img').src = srcLink;
+        instance.show();
+    }
+    return;
+});
+
+function handleEscModalClose(e) {
+
+    if (e.code === "Escape") {
+        instance.close();
+        return;
+    }
+    console.log(e);
+    
+}
